dc04e735c6b22ba2c0ac7976039614f87719996b
Abagail Hudson <abagailanne@gmail.com>
2019-06-02 18:01:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class Song
-  
+
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   extend Findable
@@ -9,7 +9,7 @@ class Song
 
   attr_accessor :name
   attr_reader :artist
-  
+
   @@songs = []
 
   def initialize
